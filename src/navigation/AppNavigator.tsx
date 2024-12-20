a08a9c10d480bb9e2a9730306bc104579ca54777
Tomi Tarvainen <ot_tomi@hotmail.com>
2024-12-20 14:47:42 +0200

    Add 'View Exercise' screen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -6,13 +6,15 @@ import AddWorkoutScreen from '../screens/AddWorkoutScreen';
 import ViewHistoryScreen from '../screens/ViewHistoryScreen';
 import ViewWorkoutScreen from '../screens/ViewWorkoutScreen';
 import AddExerciseScreen from '../screens/AddExerciseScreen';
+import ViewExerciseScreen from '../screens/ViewExerciseScreen';
 
 export type RootStackParamList = {
   Home: undefined;
-  AddWorkout: undefined;
   ViewHistory: undefined;
+  AddWorkout: undefined;
   ViewWorkout: undefined;
   AddExercise: undefined;
+  ViewExercise: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -21,10 +23,11 @@ const AppNavigator = () => (
   <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'App name(Home screen)' }} />
-      <Stack.Screen name="AddWorkout" component={AddWorkoutScreen} options={{ title: 'Add A Workout' }} />
       <Stack.Screen name="ViewHistory" component={ViewHistoryScreen} options={{ title: 'View History' }} />
+      <Stack.Screen name="AddWorkout" component={AddWorkoutScreen} options={{ title: 'Add A Workout' }} />
       <Stack.Screen name="ViewWorkout" component={ViewWorkoutScreen} options={{ title: 'View Workout' }} />
       <Stack.Screen name="AddExercise" component={AddExerciseScreen} options={{ title: 'Add A Exercise' }} />
+      <Stack.Screen name="ViewExercise" component={ViewExerciseScreen} options={{ title: 'View A Exercise' }} />
     </Stack.Navigator>
   </NavigationContainer>
 );
