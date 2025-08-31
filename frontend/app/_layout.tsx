bb655bfeabed586c3608c641d455260ac60768fd
Tomi <ot_tomi@hotmail.com>
2025-08-31 20:56:39 +0300

    Add FlashMessage to the project

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,10 +1,15 @@
 import { Stack } from 'expo-router';
+import FlashMessage from "react-native-flash-message";
 
 const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      {/* @ts-ignore */}
+      <FlashMessage position="top" />
+    </>
   );
 }
 
