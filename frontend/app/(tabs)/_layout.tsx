5cbefa1fa659e177877c2565b8b6bd0c67b84758
Tomi <ot_tomi@hotmail.com>
2025-09-02 21:20:48 +0300

    Add emojies to _layout

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,15 +1,16 @@
 import { Tabs } from 'expo-router';
+import { Text } from 'react-native';
 
 const TabsLayout = () => {
   return (
     <Tabs>
-        <Tabs.Screen name="home-screen" options={{ title: 'Home' }} />
-        <Tabs.Screen name="create-exercise-screen" options={{ title: 'Create Exercise' }} />
-        <Tabs.Screen name="create-workout-screen" options={{ title: 'Create Workout' }} />
-        <Tabs.Screen name="view-exercise-screen" options={{ title: 'View Exercise' }} />
-        <Tabs.Screen name="view-workout-screen" options={{ title: 'View workout' }} />
-        <Tabs.Screen name="view-history-screen" options={{ title: 'View history' }} />
-        <Tabs.Screen name="track-workout-screen" options={{ title: 'Track workout' }} />
+        <Tabs.Screen name="home-screen" options={{ title: 'Home', tabBarIcon: () => <Text>🏠</Text> }} />
+        <Tabs.Screen name="create-exercise-screen" options={{ title: 'Create Exercise', tabBarIcon: () => <Text>💪</Text> }} />
+        <Tabs.Screen name="create-workout-screen" options={{ title: 'Create Workout', tabBarIcon: () => <Text>📋</Text> }} />
+        <Tabs.Screen name="view-exercise-screen" options={{ title: 'View Exercise', tabBarIcon: () => <Text>📖</Text> }} />
+        <Tabs.Screen name="view-workout-screen" options={{ title: 'View workout', tabBarIcon: () => <Text>🏋️</Text> }} />
+        <Tabs.Screen name="view-history-screen" options={{ title: 'View history', tabBarIcon: () => <Text>📊</Text> }} />
+        <Tabs.Screen name="track-workout-screen" options={{ title: 'Track workout', tabBarIcon: () => <Text>📋</Text> }} />
     </Tabs>
   );
 }
